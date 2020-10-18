297bef74535a4ea7be384df1515aa0c67d15b0de
tech4minds <info@tech4minds.com>
2020-10-19 02:56:51 +0500

    first commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebAPiConsuming.Models;
+
+namespace WebAPiConsuming.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            IEnumerable<Employee> employees = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44330/api/");
+                var responseTask = client.GetAsync("Employee");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<Employee>>();
+                    readTask.Wait();
+                    employees = readTask.Result;
+                }
+                else
+                {
+                    //Error response received   
+                    employees = Enumerable.Empty<Employee>();
+                    ModelState.AddModelError(string.Empty, "Server error try after some time.");
+                }
+            }
+            return View(employees);
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPiConsuming.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+    }
+}
